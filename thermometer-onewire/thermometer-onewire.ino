5b44ea432f92c0f818e82cac9ac6246f7377bb8a
Joe Acosta <josepha48@gmail.com>
2017-03-03 16:13:08 -0800

    updates

--- thermometer-onewire/thermometer-onewire.ino
+++ thermometer-onewire/thermometer-onewire.ino
@@ -29,6 +29,7 @@ Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 DeviceAddress insideThermometer;
 
 boolean debugMode = false;
+long index;
 
 void setup()
 {
@@ -56,21 +57,31 @@ void setup()
 
   sensors.begin();
 
+  if ( sensors.getDeviceCount() > 0 ) { 
+    index = 0;
+  }
+
  if ( debugMode ) { 
    Serial.begin(9600);
 
    Serial.print("Dallas Temperature Sensor - DS18B20. Found ");
     Serial.print(sensors.getDeviceCount(), DEC);
     Serial.println(" sensors.");
-
+ }
     if (!sensors.getAddress(insideThermometer, 0)) {
       Serial.println("Unable to find address for Device 0"); 
+    } else {
+
+      sensors.setResolution(insideThermometer, 12);
+      if ( debugMode ) { 
+        Serial.print("Device 0 Resolution: ");
+        Serial.print(sensors.getResolution(insideThermometer), DEC); 
+        Serial.println();
+      
+        Serial.print("Parasite mode?: ");
+        Serial.println(sensors.getUserData(insideThermometer), DEC);
+      }
     }
-    
-    Serial.print("Device 0 Resolution: ");
-    Serial.print(sensors.getResolution(insideThermometer), DEC); 
-    Serial.println();
-  }
   delay(2000);
 }
 
@@ -165,16 +176,118 @@ void writeString(long value, char type, long writeDot)
   delay(1000);    
 }
 
+void raw() {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+  
+  if ( !oneWire.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    oneWire.reset_search();
+    delay(250);
+    return;
+  }
+  
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  } 
+
+  oneWire.reset();
+  oneWire.select(addr);
+  oneWire.write(0x44, 1);        // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a oneWire.depower() here, but the reset will take care of it.
+  
+  present = oneWire.reset();
+  oneWire.select(addr);    
+  oneWire.write(0xBE);         // Read Scratchpad
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = oneWire.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  RAW = ");
+  Serial.print((float)raw);
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+}
 
 void loop()
 {
+  
+  //raw();
+  //delay(1000);
+  //return;
   // get temperatures
-  sensors.requestTemperatures();
+  //sensors.requestTemperatures();
+  sensors.requestTemperaturesByIndex(index);
    
-  // now print out the temperature (16.7)
-  //converting from 10 mv per degree with 500 mV offset
-  // I'm using 430 as I have a lower voltage?
-  long temperatureC = (long)(sensors.getTempCByIndex(0) * 10);
+  // get C temp
+  long temperatureC = (long)(sensors.getTempCByIndex(index) * 10);
   if ( debugMode ) {
     Serial.println( "C temperature" );
     Serial.println( temperatureC, DEC );
@@ -182,8 +295,8 @@ void loop()
   writeString( temperatureC, 'C', 1L );
   delay( 2500 );
 
-  // now convert to Fahrenheit (62)
-  long temperatureF = (long)( ( ( ( ( temperatureC / 10 ) * 9 ) / 5 ) + 32 ) * 10);
+  // get F temp
+  long temperatureF = (long)(sensors.getTempFByIndex(index)*10);
   if ( debugMode ) {
       Serial.println( "F temperature" );
       Serial.println( temperatureF );
