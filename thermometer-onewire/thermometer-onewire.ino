ebca14db51961273ca97b161b1b06aff2398b187
Joe <josepha48@gmail.com>
2017-03-18 21:58:59 -0700

    add another way of getting temp, needs testing

--- thermometer-onewire/thermometer-onewire.ino
+++ thermometer-onewire/thermometer-onewire.ino
@@ -276,9 +276,55 @@ void raw() {
   Serial.println(" Fahrenheit");
 }
 
+float getCTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
+
+  byte data[12];
+  byte addr[8];
+
+  if ( !oneWire.search(addr)) {
+      //no more sensors on chain, reset search
+      oneWire.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print("Device is not recognized");
+      return -1000;
+  }
+
+  oneWire.reset();
+  oneWire.select(addr);
+  oneWire.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = oneWire.reset();
+  oneWire.select(addr);    
+  oneWire.write(0xBE); // Read Scratchpad
+
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = oneWire.read();
+  }
+
+  oneWire.reset_search();
+
+  byte MSB = data[1];
+  byte LSB = data[0];
+
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+
+  return (TemperatureSum * 18 + 5)/10 + 32;
+}
+
 void loop()
 {
   
+  //Serial.println( getCTemp() );
   //raw();
   //delay(1000);
   //return;
