430dca67c8d97fa60fb0005b7129caf946ce4a19
Joe <josepha48@gmail.com>
2023-07-04 20:28:16 -0700

    update for display

--- distance_sensor/code.py
+++ distance_sensor/code.py
@@ -3,11 +3,18 @@ import digitalio
 import time
 from time import sleep
 from analogio import AnalogIn
+from adafruit_ht16k33.segments import Seg7x4
 
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
 distance = AnalogIn(board.A1)
+
+# Seven Segment FeatherWing setup
+i2c = board.I2C()  # uses board.SCL and board.SDA
+display = Seg7x4(i2c, address=0x70)
+display.brightness = 15
+display.fill(0)  # Clear the display
 
 while True:
     voltage = ( distance.value * 3.3 ) / 65536
