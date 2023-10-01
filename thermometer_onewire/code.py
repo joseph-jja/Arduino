1a35a2fe3e1d9b0aa87102f29879cddb301f1846
Joe Acosta <joe.acosta@sephora.com>
2023-10-01 15:26:17 -0700

    add button support

--- thermometer_onewire/code.py
+++ thermometer_onewire/code.py
@@ -6,6 +6,14 @@ from busio import I2C
 from adafruit_onewire.bus import OneWireBus
 import adafruit_ds18x20
 from math import floor
+from digitalio import DigitalInOut, Direction, Pull
+
+resistor_pin = DigitalInOut(board.D6)
+resistor_pin.direction = Direction.INPUT
+
+high_pin = DigitalInOut(board.D4)
+high_pin.direction = Direction.OUTPUT
+high_pin.value = True
 
 ow_bus = OneWireBus(board.D5)
 print("starting...")
@@ -14,7 +22,6 @@ print("starting...")
 i2c = I2C(board.D1, board.D0)
 
 if i2c.try_lock():
-
     for x in i2c.scan():
         print("testing", x, hex(x))
     i2c.unlock()
@@ -32,7 +39,9 @@ for device in devices:
 
 ds18b20 = adafruit_ds18x20.DS18X20(ow_bus, devices[0])
 
+i = 0
 while True:
+    
     # so I don't know if this is the correct formula
     # it seems to be better than what I read :)
     celsius = ds18b20.temperature
@@ -40,22 +49,35 @@ while True:
 
     celsius = floor(celsius * 10) / 10
     fahrenheit = floor(fahrenheit * 10) / 10
-    
+
     celsiusStr = str(celsius) + "C"
     fahrenheitStr = str(fahrenheit) + "F"
-    if celsius > 100: 
+    if celsius > 100:
         celsius = floor(celcius)
         celsiusStr = str(celsius) + "C"
-    
-    if fahrenheit > 100: 
+
+    if fahrenheit > 100:
         fahrenheit = floor(fahrenheit)
         fahrenheitStr = str(fahrenheit) + "F"
-    
+
     print("Got temperature C", celsius)
     print("Got temperature F", fahrenheit)
     display.fill(0)
+
+    if resistor_pin.value == True:
+        print ("Got input button", resistor_pin.value)
+        i = 1
+        #resistor_pin.value = False
     
-    display.print(celsiusStr)
-    sleep(1)
-    display.print(fahrenheitStr)
-    sleep(1)
+    if i > 0:
+        display.print(celsiusStr)
+        sleep(1)
+        display.print(fahrenheitStr)
+        sleep(1)
+        i = i + 1
+        
+    if i > 20:
+        i = 0
+        
+    print("Value of i is", i)
+    print("Value of i is", i)
