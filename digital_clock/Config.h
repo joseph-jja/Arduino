b221b07a5e492fb460b466465ed098bdb7dbc9de
Joe A <josepha48.nospam@gmail.com>
2025-04-26 15:41:48 -0700

    change code for esp8266 to set time

---
+++ digital_clock/Config.h
@@ -0,0 +1,6 @@
+// wifi change these
+const char* ssid     = "****************";
+const char* password = "***********";
+IPAddress local_IP(192, 168, 0, 1);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
