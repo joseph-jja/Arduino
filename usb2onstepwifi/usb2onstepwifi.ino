9cb1d8ba259575c4e43e7d6dd8471c26c9ec30a1
Joe A <josepha48.nospam@gmail.com>
2025-09-10 12:44:43 -0700

    initial code to talk to onstep device via 8266

---
+++ usb2onstepwifi/usb2onstepwifi.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+
+#define STATION_ID "host"
+#define STATION_PWD "password"
+
+const char* ssid     = STATION_ID;
+const char* password = STATION_PWD;
+
+IPAddress gateway;
+WiFiClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  gateway = WiFi.gatewayIP();
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP().toString());
+  Serial.print("gateway address: ");
+  Serial.println(gateway.toString());
+}
+
+void loop() {
+  
+   String host_str = gateway.toString();
+   const char* host = host_str.c_str();
+   const uint16_t port = 9999;
+
+    // Use WiFiClient class to create TCP connections
+    if (!client.connected()) {
+      Serial.print("connecting to ");
+      Serial.print(host_str);
+      Serial.print(' ');
+      Serial.print(host);
+      Serial.print(':');
+      Serial.println(port);
+
+      if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        delay(5000);
+        return;
+      }
+    }
+    delay(10);
+
+    if (client.connected()) {
+
+      while (Serial.available()) {
+        Serial.println('Data being read');
+        client.write(Serial.read());
+      }
+
+      while (client.available()) {
+        Serial.write(client.read());
+      }
+
+      client.flush();
+    }
+}
