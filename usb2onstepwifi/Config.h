c875c2ef7ef85863dcb803b98b2c0f0131454970
josepha48 <josepha48@gmail.com>
2025-09-11 20:37:17 -0700

    skip over lat long for focuser code

---
+++ usb2onstepwifi/Config.h
@@ -0,0 +1,10 @@
+
+
+#define STATION_ID "xxx"
+#define STATION_PWD "xxx"
+
+// about the center of the US
+const char* DEFAULT_LATITUDE = "+38:80";
+const char* DEFAULT_LONGITUDE = "+106:50";
+
+int utcoffset = -5;
