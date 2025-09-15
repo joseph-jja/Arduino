e90bfad25a3772eb26eecd576776ec87f2729f58
josepha48 <josepha48@gmail.com>
2025-09-14 20:17:04 -0700

    add in string functions for both

--- usb2onstepwifi/usb2onstepwifi.ino
+++ usb2onstepwifi/usb2onstepwifi.ino
@@ -3,7 +3,7 @@
 #include <Wire.h>
 
 #include "Config.h"
-#include "Constants.h"
+#include "string_functions.h"
 
 //#define USB_DEBUG_ENABLED 1
 //#define USE_I2C_CHANNEL 1
@@ -133,16 +133,6 @@ void setup() {
   }
 
   Serial.flush();
-}
-
-// compares 2 string
-boolean compare(char *commandIn, char *commandStr) {
-  return (memcmp(commandIn, commandStr, strlen(commandStr)) == 0);
-}
-
-// copies part of a string from source to destination
-void substring(char *source, int start_index, int length, char *destination) {
-  strncpy(destination, source + start_index, length);
 }
 
 /*
@@ -332,26 +322,6 @@ boolean read_in_usb_data() {
 
   Serial.flush();
   return check_override(bufferIn);
-}
-
-boolean has_reply(char *messageIn) {
-
-  boolean hasResponse = true;
-  // we know that :S is setting something and reply is 0 or 1
-  // we know that :G is getting something so these have a reply message
-  if (compare(messageIn, ":S") || compare(messageIn, ":G")) {
-    hasResponse = true;
-  } else {
-    int i = 0;
-    while (i < NUMBER_OF_STRINGS && hasResponse) {
-      if (compare(messageIn, COMMANDS_WITH_NO_REPLY[i])) {
-        hasResponse = false;
-      }
-      i++;
-    }
-  }
-
-  return hasResponse;
 }
 
 void read_in_wifi_data(char *messageIn) {
