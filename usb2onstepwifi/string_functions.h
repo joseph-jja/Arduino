fa195416b429f6aa6faf6cde381379abd3b90c19
Joe <josepha48@gmail.com>
2025-09-15 17:22:54 -0700

    Add new function

--- usb2onstepwifi/string_functions.h
+++ usb2onstepwifi/string_functions.h
@@ -5,3 +5,5 @@ bool compare(char *commandIn, char *commandStr);
 void substring(char *source, int start_index, int length, char *destination);
 
 bool has_reply(char *messageIn);
+
+bool boolean_reply(char *commandIn);
