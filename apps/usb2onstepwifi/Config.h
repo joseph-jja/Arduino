a750c5161cba44b525d87246e64fc3eaa083b39b
Joe A <josepha48.nospam@gmail.com>
2025-10-01 12:18:14 -0700

    ad in MOUNT_MODE

--- apps/usb2onstepwifi/Config.h
+++ apps/usb2onstepwifi/Config.h
@@ -8,3 +8,9 @@
 #define DEFAULT_LONGITUDE "+106:50"
 
 #define DEFAULT_UTC_OFFSET 5
+
+// // A If scope in AltAz Mode
+// D If scope is currently in the Downloader [Autostar II & Autostar]
+// L If scope in Land Mode
+// P If scope in Polar Mode
+#define MOUNT_MODE P
