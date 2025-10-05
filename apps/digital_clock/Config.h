65735dca0658d3a1a4ed2ad7611fb5dc50f1f4dd
Joe <josepha48@gmail.com>
2025-10-04 18:35:32 -0700

    Add mode switch

--- apps/digital_clock/Config.h
+++ apps/digital_clock/Config.h
@@ -4,3 +4,9 @@ const char* password = "***********";
 IPAddress local_IP(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
+
+#define CLIENT_MODE 0
+#define SERVER_MODE 1
+
+// define this to either SERVER_MODE or CLIENT_MODE
+#define RUNNING_MODE SERVER_MODE
