c9922ca5e88593bc4a3e3443c8e05d257307d5c4
Joe <josepha48@gmail.com>
2025-10-04 18:37:48 -0700

    Just define one more

--- apps/digital_clock/Config.h
+++ apps/digital_clock/Config.h
@@ -5,8 +5,7 @@ IPAddress local_IP(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-#define CLIENT_MODE 0
-#define SERVER_MODE 1
-
 // define this to either SERVER_MODE or CLIENT_MODE
-#define RUNNING_MODE SERVER_MODE
+// defining both results in error
+//#define CLEINT_MODE 1
+#define SERVER_MODE 1
