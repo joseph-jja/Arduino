63e0f38efcb16bfb7a5ace58b24aa54961f4de93
Joe A <josepha48.nospam@gmail.com>
2025-10-13 12:58:15 -0700

    update plugin

--- apps/onstepxMountPlugin/lib/Datetime.h
+++ apps/onstepxMountPlugin/lib/Datetime.h
@@ -2,6 +2,21 @@
 
 #define DATE_TIME_SIZE 10
 
+// store time in 12 hour format with AM/PM
+typedef struct {
+    int hour;
+    int minute;
+    int second;
+    char meridian[3]; // Stores "AM" or "PM"
+} TimeInfo;​
+
+// store date split out
+typedef struct {
+    int year;
+    int month;
+    int day;
+} DateInfo;​
+
 class Datetime {
 public:
   // Public members (accessible from outside the class)
@@ -19,14 +34,17 @@ private:
   long update_time(unsigned long ms);
   long update_time_value(char time[], unsigned long ms, long time_in, bool is_twenty_four_hour);
 
+  TimeInfo convert_24h_to_12hr(int hour_24, int minute, int second);
+  TimeInfo convert_12h_to_24hr(int hour_12, int minute, int second, const char* meridian);
+
   long start_time;
   long delta_time;
   long days_changed;
 
   // date and time stuff
-  long date_part;
-  long local_time_part;
-  long sidereal_time_part;
+  DateInfo date_part;
+  TimeInfo local_time_part;
+  TimeInfo sidereal_time_part;
 };
 
 extern Datetime datetime;
