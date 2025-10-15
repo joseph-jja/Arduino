46afb261428bd5ddcae609f6c21162a80f074b72
Joe <josepha48@gmail.com>
2025-10-14 18:35:14 -0700

    Update OnstepXMountPlugin.h

--- apps/onstepxMountPlugin/OnstepXMountPlugin.h
+++ apps/onstepxMountPlugin/OnstepXMountPlugin.h
@@ -1,6 +1,10 @@
 // USB2I2C plugin
 #pragma once
 
+#include "lib/location_functions.h"
+#include "lib/datetime_functions.h"
+#include "lib/site_functions.h"
+
 #include "../../lib/commands/CommandErrors.h"
 
 #define DEFAULT_LOCATION_SIZE 20
@@ -14,6 +18,13 @@ public:
   void loop();
 
   bool command(char reply[], char command[], char parameter[], bool *supressFrame, bool *numericReply, CommandError *commandError);
+
+private:
+
+     Site site;
+     Location location;
+     Datetime datetime;
+    
 };
 
 extern OnStepXMountPlugin onstepxMountPlugin;
