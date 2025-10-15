07da1daaa90a2e617b37dad95d05f91d6049585e
josepha48 <josepha48@gmail.com>
2025-10-14 20:55:44 -0700

    update to remove classes

--- apps/onstepxMountPlugin/Config.h
+++ apps/onstepxMountPlugin/Config.h
@@ -9,6 +9,9 @@
 
 #define DEFAULT_UTC_OFFSET "+5"
 
+#define DEFAULT_DATE "08/15/25"
+#define DEFAULT_TIME "12:15:30"
+
 // // A If scope in AltAz Mode
 // D If scope is currently in the Downloader [Autostar II & Autostar]
 // L If scope in Land Mode
