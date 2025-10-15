dd73b658a044c6994fa953a9a4af54498e100b25
Joe <josepha48@gmail.com>
2025-10-14 18:27:18 -0700

    Update site_functions.h

--- apps/onstepxMountPlugin/lib/site_functions.h
+++ apps/onstepxMountPlugin/lib/site_functions.h
@@ -1,1 +1,10 @@
+#pragma once
 
+#include "Location.h"
+
+typedef struct {
+    char sitename[128]; 
+    SiteLocation siteLocation;
+} SiteInfo;
+
+bool addSite(char* siteName, char* latitude, char* longitude, char* utcOffset, int siteNumber);
