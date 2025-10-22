12939285785404220a6b1c0fc59ddc95c0db14d7
Joe <josepha48@gmail.com>
2025-10-21 18:44:22 -0700

    Update interval

---
+++ apps/digital_clock_client/Config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// wifi change these
+const char* ssid     = "****************";
+const char* password = "***********";
+
+// client default update interval is 15m
+#define UPDATE_INTERVAL  900000
