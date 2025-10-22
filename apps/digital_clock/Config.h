180fee0413cacf4b970640c8b3129d236195d132
Joe <josepha48@gmail.com>
2025-10-21 18:45:04 -0700

    Delete UPDATE_INTERVAL definition from Config.h
    Removed the default update interval definition.

--- apps/digital_clock/Config.h
+++ apps/digital_clock/Config.h
@@ -6,6 +6,3 @@ const char* password = "***********";
 IPAddress local_IP(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
-
-// client default update interval is 15m
-#define UPDATE_INTERVAL  900000
