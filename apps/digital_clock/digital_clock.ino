374e391dceda10e701db98d46ef8dbb1f79333a1
Joe <josepha48@gmail.com>
2025-10-21 18:35:07 -0700

    Remove client mode
    TODO test code updates

--- apps/digital_clock/digital_clock.ino
+++ apps/digital_clock/digital_clock.ino
@@ -18,16 +18,6 @@
 #include "Config.h"
 #include "Constants.h"
 
-#if defined(SERVER_MODE) && defined(CLIENT_MODE)
-#error "ERROR: Only one option of SERVER_MODE or CLIENT_MODE can be defined."
-#endif
-
-#ifndef SERVER_MODE
-#ifndef CLIENT_MODE
-#error "ERROR: Either SERVER_MODE or CLIENT_MODE must be defined."
-#endif
-#endif
-
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 
 #define SERIAL_BAUD 115200
@@ -44,14 +34,8 @@ static time_t rawtime;
 long currentHour,
   currentMinute;
 
-#ifdef SERVER_MODE
 // Create AsyncWebServer object on port 80
 ESP8266WebServer server(80);
-#else
-#ifdef CLIENT_MODE
-WiFiClient client;
-#endif
-#endif
 
 void setup_builtin_pin() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -65,7 +49,6 @@ void blink_pin(int sleep_time) {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-#ifdef SERVER_MODE
 void handleIndex() {
   Serial.print("Request for index page");
   Serial.println(WiFi.softAPIP());
@@ -136,7 +119,6 @@ void handleTimeRequest() {
     server.send(200, "text/plain", "ERROR");
   }
 }
-#endif
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
@@ -165,7 +147,6 @@ void setup() {
   }
   Serial.println("Display initialized!");
 
-#ifdef SERVER_MODE
   // clock found so initialize to current data time from computer
   if (rtc.begin()) {
     Serial.println("Begin RTC!");
@@ -190,7 +171,6 @@ void setup() {
   Serial.print("WiFi with IP address: ");
   Serial.println(IP);
 
-
   // Routes for web server
   server.on("/", handleIndex);
   server.on("/update", handleUpdate);
@@ -203,127 +183,7 @@ void setup() {
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
-#else
-#ifdef CLIENT_MODE
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  //  WiFi.onStationModeDisconnected(onWifiDisconnect);
-  //  WiFi.onStationModeConnected(onWifiConnect);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
-  WiFi.setSleepMode(WIFI_NONE_SLEEP);
-
-  gateway = WiFi.gatewayIP();
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP().toString());
-  Serial.print("gateway address: ");
-  Serial.println(gateway.toString());
-
-#endif
-#endif
 }
-
-#ifdef CLIENT_MODE
-
-bool connect_client() {
-
-  String host_str = gateway.toString();
-  const char *host = host_str.c_str();
-  int port = 80;
-
-  int i = 0;
-  bool isConnected = false;
-  while (!isConnected && i < 10) {
-    isConnected = client.connect(host, port);
-    if (!isConnected) {
-      Serial.println("Cnnection failed");
-    }
-    i++;
-  }
-  Serial.print("Connected? ");
-  Serial.print(isConnected);
-  Serial.print(" to ");
-  Serial.print(host_str);
-  Serial.print(" on port ");
-  Serial.println(port);
-}
-
-
-long lastUpdate = 0;
-char time_buffer[TIME_BUFFER_SIZE];
-
-void updateClient() {
-  if (lastUpdate == 0) {
-    lastUpdate = millis();
-  }
-
-  int i = 0;
-  memset(time_buffer, '\0', TIME_BUFFER_SIZE);
-  if ((millis() - lastUpdate) > UPDATE_INTERVAL) {
-    bool isConnected = connect_client();
-    long start_time = millis();
-    bool foundEnd = false;
-    while (!foundEnd) {
-      if ((millis() - start_time) < UPDATE_INTERVAL) {
-        if (client.available()) {
-          char incomingByte = client.read();
-          Serial.print("WIFI data read in ");
-          Serial.println(incomingByte);
-          if (incomingByte != NULL && isprint(incomingByte)) {
-            time_buffer[i] = incomingByte;
-            i++;
-          }
-          if (incomingByte == '#') {
-            foundEnd = true;
-          }
-        }
-      } else {
-        delay(5);
-        start_time = millis();
-      }
-    }
-    client.stop();
-    lastUpdate = millis();
-  }
-
-  // TODO parse string
-  // update  rawtime struct
-  // update currentHour and currentMinute for display
-  char hours[3];
-  char minutes[3];
-  bool isMinutes = false;
-  bool keepChar = true;
-  int h = 0, m = 0;
-  if (time_buffer != NULL && strlen(time_buffer) > 3) {
-    for (int i = 0; i < strlen(time_buffer) - 1; i++) {
-      if (time_buffer[i] == ':' || time_buffer[i] == '#') {
-        isMinutes = true;
-        keepChar = false;
-      }
-      if (keepChar && isMinutes) {
-        minutes[m] = time_buffer[i];
-        m++;
-      } else if (keepChar) {
-        hours[h] = time_buffer[i];
-        h++;
-      }
-      keepChar = true;
-    }
-    Serial.print("Hours ");
-    Serial.print(hours);
-    Serial.print("and minutes ");
-    Serial.println(minutes);
-  }
-}
-#endif
 
 void get_time() {
 
@@ -364,7 +224,6 @@ void get_time() {
     now->tm_sec = rtnow.second();
     rawtime = mktime(now);
   }
-
 
   currentHour = now->tm_hour;
   currentMinute = now->tm_min;
@@ -421,23 +280,14 @@ void writeString() {
 
 void loop() {
 
-  char buff[255];
-
-#ifdef CLIENT_MODE
-  updateClient();
-#endif
-
-#ifdef SERVER_MODE
   get_time();
-#endif
 
   writeString();
 
   blink_pin(100);
-#ifdef SERVER_MODE
+
   server.handleClient();
   MDNS.update();
-#endif
 
   delay(100);
 }
