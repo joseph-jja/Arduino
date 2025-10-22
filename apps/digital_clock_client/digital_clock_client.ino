e5c7f9ae43f9dbcd4c294270658a90bec0cd640a
Joe <josepha48@gmail.com>
2025-10-21 18:57:17 -0700

    Improve WiFi connection handling and logging
    Refactor WiFi connection setup and enhance logging.

--- apps/digital_clock_client/digital_clock_client.ino
+++ apps/digital_clock_client/digital_clock_client.ino
@@ -8,16 +8,29 @@
 #include <ESP8266WiFi.h>
 #include <Hash.h>
 
-void setup() {
+IPAddress gateway;
+WiFiClient client;
 
+Serial.begin(9600);
+
+  setup_builtin_pin();
+
+  println("");
+  println("");
+  print("Connecting to ");
+  println(ssid);
+
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  //  WiFi.onStationModeDisconnected(onWifiDisconnect);
-  //  WiFi.onStationModeConnected(onWifiConnect);
+  WiFi.onStationModeDisconnected(onWifiDisconnect);
+  WiFi.onStationModeConnected(onWifiConnect);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    print(".");
   }
 
   WiFi.setAutoReconnect(true);
@@ -25,14 +38,20 @@ void setup() {
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
 
   gateway = WiFi.gatewayIP();
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP().toString());
-  Serial.print("gateway address: ");
-  Serial.println(gateway.toString());
-}
+  println("");
+  println("WiFi connected");
+  print("IP address: ");
+  println(WiFi.localIP().toString());
+  print("gateway address: ");
+  println(gateway.toString());
 
+  for (int i = 0; i < 5; i++) {
+    blink_pin(50);
+    delay(25);
+  }
+
+  Serial.flush();
+}
 bool connect_client() {
 
   String host_str = gateway.toString();
