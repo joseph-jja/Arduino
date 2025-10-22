6bf7b861307b2e2cc192d6372f3cc643eb91607b
Joe <josepha48@gmail.com>
2025-10-21 18:30:51 -0700

    Remove server and client modes, this will be server only
    Removed CLIENT_MODE definition and enforced SERVER_MODE.

--- apps/digital_clock/Config.h
+++ apps/digital_clock/Config.h
@@ -7,10 +7,5 @@ IPAddress local_IP(192, 168, 0, 1);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-// define this to either SERVER_MODE or CLIENT_MODE
-// defining both results in error
-//#define CLIENT_MODE 1
-#define SERVER_MODE 1
-
 // client default update interval is 15m
 #define UPDATE_INTERVAL  900000
