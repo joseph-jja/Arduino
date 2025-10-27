0e1a1f6c3c1fe8580c2a07b01191373c1189a13a
Joe A <josepha48.nospam@gmail.com>
2025-10-27 13:50:04 -0700

    fix bnugs created by copilot

--- apps/onstepxMountPlugin/lib/site_functions.cpp
+++ apps/onstepxMountPlugin/lib/site_functions.cpp
@@ -3,6 +3,7 @@
 #include "stdio.h"
 
 #include "site_functions.h"
+#include "location_functions.h"
 
 bool addSite(SiteInfo* siteInfo, char* siteName, char* latitude, char* longitude, char* utcOffset, int siteNumber) {
 
@@ -15,26 +16,22 @@ bool addSite(SiteInfo* siteInfo, char* siteName, char* latitude, char* longitude
     }
 
     // Parse into temporaries so we don't partially modify siteInfo on failure
-    char latTmp[MAX_LATITUDE];
-    char lonTmp[MAX_LONGITUDE];
-    int utcTmp = 0;
-
-    // Ensure temp buffers are zeroed (optional but defensive)
-    latTmp[0] = '\0';
-    lonTmp[0] = '\0';
+    Location latTmp;
+    Location longTmp;
+    Offset utcTmp;
 
     // Parse and validate latitude
-    if (!parse_location((char*)latitude, latTmp, MAX_LATITUDE)) {
+    if (!parse_location((const char*)latitude, &latTmp, MAX_LATITUDE)) {
         return false; // Invalid latitude format
     }
 
     // Parse and validate longitude
-    if (!parse_location((char*)longitude, lonTmp, MAX_LONGITUDE)) {
+    if (!parse_location((const char*)longitude, &longTmp, MAX_LONGITUDE)) {
         return false; // Invalid longitude format
     }
 
     // Parse and validate UTC offset
-    if (!parse_offset((char*)utcOffset, &utcTmp)) {
+    if (!parse_offset((const char*)utcOffset, &utcTmp)) {
         return false; // Invalid UTC offset format
     }
 
@@ -45,13 +42,13 @@ bool addSite(SiteInfo* siteInfo, char* siteName, char* latitude, char* longitude
     snprintf(siteInfo[siteNumber].sitename, sizeof(siteInfo[siteNumber].sitename), "%s", siteName);
 
     // Copy parsed latitude/longitude into the struct, ensuring null-termination
-    strncpy(siteInfo[siteNumber].latitude, latTmp, sizeof(siteInfo[siteNumber].latitude) - 1);
-    siteInfo[siteNumber].latitude[sizeof(siteInfo[siteNumber].latitude) - 1] = '\0';
+    //strncpy(siteInfo[siteNumber].latitude, latTmp, sizeof(siteInfo[siteNumber].latitude) - 1);
+    //siteInfo[siteNumber].latitude[sizeof(siteInfo[siteNumber].latitude) - 1] = '\0';
 
-    strncpy(siteInfo[siteNumber].longitude, lonTmp, sizeof(siteInfo[siteNumber].longitude) - 1);
-    siteInfo[siteNumber].longitude[sizeof(siteInfo[siteNumber].longitude) - 1] = '\0';
+    //strncpy(siteInfo[siteNumber].longitude, lonTmp, sizeof(siteInfo[siteNumber].longitude) - 1);
+    //siteInfo[siteNumber].longitude[sizeof(siteInfo[siteNumber].longitude) - 1] = '\0';
 
-    siteInfo[siteNumber].utc_offset = utcTmp;
+    //siteInfo[siteNumber].utc_offset = utcTmp;
 
     return true; // Successfully added the site
 }
