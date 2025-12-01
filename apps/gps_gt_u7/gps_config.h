f0eb0be9618cae43dd255fd3c8b939ac8fcee501
Joe <josepha48@gmail.com>
2025-11-30 18:20:16 -0800

    Enhance GPS_LOCATION struct with comments
    Added comments to clarify the purpose of struct members.

--- apps/gps_gt_u7/gps_config.h
+++ apps/gps_gt_u7/gps_config.h
@@ -5,6 +5,15 @@
 #define GPS_RX_PIN   34
 
 typedef struct {
+    // data was updated
     bool updated;
+    
+    // gps location
+    float latitude;
+    float longitude;
+
+    // time and date 
+    char gps_date[12];
+    char gps_time[10];
 } GPS_LOCATION;
 
