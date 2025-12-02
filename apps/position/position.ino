a205ba48440a039e49650d4fc76c8ae810d50a21
Joe A <josepha48.nospam@gmail.com>
2025-12-02 15:32:03 -0800

    add in code for position

---
+++ apps/position/position.ino
@@ -0,0 +1,30 @@
+#import "mpu6050_config.h"
+#import "sdd1306_config.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  setup_accel_n_gyro();
+  setup_SDD1306();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  loop_accel_n_gyro();
+  MPU6050_PROCESSED_DATA results = getMPU6050Data();
+
+  clear_display();
+
+  char buff[32];
+  memset(buff, '\0', 32);
+  sprintf(buff, "Acc Roll %0.02f", results.accAngleX);
+  write_display(5, 5, buff);
+
+  memset(buff, '\0', 32);
+  sprintf(buff, "Acc Roll %0.02f", results.accAngleY);
+  write_display(5, 5, buff);
+
+  delay(1000);
+}
