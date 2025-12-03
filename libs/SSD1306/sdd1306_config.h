1bca3d07d1a8ee21b4376660f85344ae2f36f89f
Joe <josepha48@gmail.com>
2025-12-02 18:36:16 -0800

    Update SSD1306 configuration for I2C support

--- libs/SSD1306/sdd1306_config.h
+++ libs/SSD1306/sdd1306_config.h
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-
-#define OLED_ADDRESS 0x3C
-
+// For SSD1306: Ensure the correct address (0x3C or 0x3D) is passed
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1 // Or an unused GPIO pin, like D0 (GPIO16)
+#define SCREEN_ADDRESS 0x3C 
