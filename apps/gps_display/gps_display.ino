54078260fe1cd2b3d15c5b74bf212e6b5d1d76a4
Joe A <josepha48.nospam@gmail.com>
2026-01-29 15:34:14 -0800

    need to integrate a wifi client that can send GPS data

--- apps/gps_display/gps_display.ino
+++ apps/gps_display/gps_display.ino
@@ -2,9 +2,70 @@
 // ESP32 S3 -> Development Board N16R8 MCU
 // GPS -> gt-u7
 // I2C display -> SSD1306
+#include "Config.h"
 #include "gps_config.h"
 #include "sdd1306_config.h"
 #include <Fonts/FreeMono9pt7b.h>
+
+#include "WiFi.h"
+
+IPAddress gateway;
+
+WiFiServer server(12000);
+WiFiClient client;
+
+// connect to wifi network
+void setup_wifi_client() {
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  //WiFi.onStationModeDisconnected(onWifiDisconnect);
+  //WiFi.onStationModeConnected(onWifiConnect);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+  //WiFi.setSleepMode(WIFI_NONE_SLEEP);
+
+  gateway = WiFi.gatewayIP();
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println();
+  Serial.print("gateway address: ");
+  Serial.println(gateway.toString());
+
+  server.begin();
+  client = server.available();
+}
+
+/*
+add this to GPS code and pass in server 
+/ /so GPS can do a read and send
+void wifi_client_send_gps(WiFiServer server, WiFiClient client) {
+
+  long start = millis();
+
+  while (!client &&  && (millis() - start) < 5000){
+      if (Serial1.available() > 0){
+      data[idx] = Serial1.read();
+        if (data[idx] == LF) {
+          data[idx-1] = 0;
+//Serial.println(data);
+          delay(10);
+          server.println(data);
+          idx = -1;
+          }
+      idx++;
+      }
+    }
+}*/
 
 void setup() {
   // Initialize the serial monitor for debugging
@@ -16,6 +77,8 @@ void setup() {
   setup_SDD1306();
 
   setDisplayFont(&FreeMono9pt7b); 
+
+  setup_wifi_client();
 
 #ifdef ESP32
   BaseType_t coreID = xPortGetCoreID();
@@ -42,7 +105,7 @@ void loop() {
   loop_gps();
 
   displayInfo();
-  delay(1000);
+
 }
 
 void displayInfo() {
@@ -58,6 +121,7 @@ void displayInfo() {
     // You may want to reset the ESP32 here if no data is received for a long time
     write_display(2, 16, "No GPS\ndata!");
     show_display();
+    delay(2500);
     return;
   }
 
@@ -105,7 +169,7 @@ void displayInfo() {
   write_display(100 - (strlen(buffer) * 12), 61, buffer);  
 
   show_display();
-  delay(5000);
+  delay(10000);
 
   clear_display();
   write_display(2, 11, "Date:");
@@ -114,5 +178,5 @@ void displayInfo() {
   write_display(30, 61, myGPS.gps_time);  
 
   show_display();
-  delay(2500);
+  delay(3500);
 }
