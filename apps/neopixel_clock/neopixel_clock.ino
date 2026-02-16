eaec627ba4195425e376138c455d2dda029b5ef8
Joe <josepha48@gmail.com>
2026-02-16 13:12:15 -0800

    Refactor setSinglePixelColor function to use color array

--- apps/neopixel_clock/neopixel_clock.ino
+++ apps/neopixel_clock/neopixel_clock.ino
@@ -3,6 +3,10 @@
 
 #include <Adafruit_NeoPixel.h>
 
+static int RED[3] = {255, 0, 0}; 
+static int GREEN[3] = {0, 255, 0}; 
+static int BLUE[3] = {0, 0, 255}; 
+
 #define HOURS_ONE 18
 #define HOURS_TWO 17
 #define MINUTES_ONE 16
@@ -47,83 +51,12 @@ void setup() {
   pixels_seconds_2.show();
 }
 
-void setSinglePixelColor(Adafruit_NeoPixel pixels, int i, int cycle) {
+void setSinglePixelColor(Adafruit_NeoPixel pixels, int color[], int pixel_index) {
 
-  int red = 255,
-      green = 0,
-      blue = 0;
-
-  switch (cycle) {
-    // red
-    case 0:
-      red = 255;
-      green = 0;
-      blue = 0;
-      break;
-    // orange
-    case 1:
-      red = 255;
-      green = 165;
-      blue = 0;
-      break;
-    // yellow
-    case 2:
-      red = 255;
-      green = 255;
-      blue = 0;
-      break;
-    // green
-    case 3:
-      red = 0;
-      green = 128;
-      blue = 0;
-      break;
-    // blue
-    case 4:
-      red = 0;
-      green = 0;
-      blue = 255;
-      break;
-    // purple
-    case 5:
-    default:
-      red = 128;
-      green = 0;
-      blue = 128;
-      break;
-  }
-
-  pixels.setPixelColor(i, red, green, blue);
+  pixels.setPixelColor(pixel_index, color[0], color[1], color[2]);
 }
 
-int xcycle = 0;
-
 void loop() {
-  int i,
-      j = 0;
 
-  for (i = 0; i < PIXEL_CT; i++) {
-    // red 255, 0, 0
-    // green 0, 255, 0
-    // blue 0, 0, 255
-    int x = (j < 5 ) ? j + 1 : 0, 
-      y = (x < 5 ) ? x + 1 : 0, 
-      z = (y < 5 ) ? y + 1 : 0, 
-      a = (z < 5 ) ? z + 1 : 0;
-    setSinglePixelColor(pixels_top, i, ((j + xcycle) % 6));
-    setSinglePixelColor(pixels_left, i, ((x + xcycle) % 6));
-    setSinglePixelColor(pixels_b_left, i, ((y + xcycle) % 6));
-    setSinglePixelColor(pixels_b_right, i, ((z + xcycle) % 6));
-    setSinglePixelColor(pixels_right, i, ((a + xcycle) % 6));
-    j++;
-    if ( j > 6 ) {
-      j = 0;
-    }
-    delay(250);
-  }
-
-  pixels_top.show();
-  delay(250);
-  xcycle++;
-
+  
 }
