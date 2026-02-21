8fcf1946d3bd92b88b3c9ecce5366b60b39bed2b
Joe <josepha48@gmail.com>
2026-02-20 18:10:44 -0800

    Add configuration for WiFi settings in Config.h

---
+++ apps/neopixel_clock/Config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// wifi change these
+const char* ssid     = "ESP-CLOCK-BINARY-AP-001";
+const char* password = "ESP-CLOCK-BINARY-AP-001";
+IPAddress local_IP(192, 168, 0, 1);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
