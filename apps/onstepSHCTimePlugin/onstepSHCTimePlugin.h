f54c66f58846813097bb46719ba1b88e33c491c4
Joe <josepha48@gmail.com>
2026-03-07 10:03:17 -0800

    Rename OnStepXMountPlugin to OnstepSHCTimePlugin

---
+++ apps/onstepSHCTimePlugin/onstepSHCTimePlugin.h
@@ -0,0 +1,36 @@
+
+// USB2I2C plugin
+#pragma once
+
+#include "lib/location_functions.h"
+#include "lib/datetime_functions.h"
+#include "lib/site_functions.h"
+
+#include "../../lib/commands/CommandErrors.h"
+
+#define RESPONSE_BUFFER_SIZE 50
+
+class OnstepSHCTimePlugin {
+
+public:
+  // the initialization method must be present and named: void init();
+  void init();
+
+  void loop();
+
+  bool command(char reply[], char command[], char parameter[], bool *supressFrame, bool *numericReply, CommandError *commandError);
+
+private:
+
+     char response_buffer[RESPONSE_BUFFER_SIZE];
+
+     SiteInfo   siteInfo[4];
+     Location   latitude;
+     Location   longitude;
+     Offset     utc_offset;
+     DateInfo   dateInfo;
+     TimeInfo   timeInfo;
+     TimeInfo   siderealTimeInfo;
+};
+
+extern OnstepSHCTimePlugin onstepSHCTimePlugin;
