cf71bf464fbe61cc62796f1cd84bdaeb71fa374d
Joe A <josepha48.nospam@gmail.com>
2026-07-21 15:34:44 -0700

    add in matter code to enable connecting to wifi and be matter device with QR code

--- apps/matter_reed_switch/matter_reed_switch.ino
+++ apps/matter_reed_switch/matter_reed_switch.ino
@@ -1,73 +1,122 @@
 // Matter Manager
 #include <Arduino.h>
 #include <Matter.h>
+#undef CONFIG_ENABLE_CHIPOBLE
 #if !CONFIG_ENABLE_CHIPOBLE
 // if the device can be commissioned using BLE, WiFi is not used - save flash space
 #include <WiFi.h>
 #endif
 #include "esp_sleep.h"
 
+#include "config.h"
+
 #define ESP32_S3_ENABLED true
 
 // List of Matter Endpoints for this Node
 // Matter Contact Sensor Endpoint
-MatterContactSensor ContactSensor;
+MatterContactSensor ContactSensor = MatterContactSensor();
 
 // Use an RTC GPIOus
 
-#define SENSOR_PIN 5 
+#define SENSOR_PIN 5
 
 // CONFIG_ENABLE_CHIPOBLE is enabled when BLE is used to commission the Matter Network
 #if !CONFIG_ENABLE_CHIPOBLE
 // WiFi is manually set and started
-const char *ssid = "your-ssid";          // Change this to your WiFi SSID
-const char *password = "your-password";  // Change this to your WiFi password
+const char *ssid = WIFI_SSID;          // Change this to your WiFi SSID
+const char *password = WIFI_PASSWORD;  // Change this to your WiFi password
 #endif
- 
+
 // LED will be used to indicate the Contact Sensor state
 // set your board RGB LED pin here
 #ifdef RGB_BUILTIN
 const uint8_t ledPin = RGB_BUILTIN;
 #else
-const uint8_t ledPin = 2;  // Set your pin here if your board has not defined LED_BUILTIN
+const uint8_t ledPin = 48;  // Set your pin here if your board has not defined LED_BUILTIN
 #warning "Do not forget to set the RGB LED pin"
 #endif
 
-const uint8_t buttonPin = BOOT_PIN;  // Set your pin here. Using BOOT Button.
+const uint8_t buttonPin = SENSOR_PIN;  // Set your pin here. Using BOOT Button.
 
 // Button control
 uint32_t button_time_stamp = 0;                // debouncing control
-bool button_state = false;                     // false = released | true = pressed
+bool button_state = true;                      // false = released | true = pressed
 const uint32_t debouceTime = 250;              // button debouncing time (ms)
 const uint32_t decommissioningTimeout = 5000;  // keep the button pressed for 5s, or longer, to decommission
 
 // look here for code example https://docs.espressif.com/projects/arduino-esp32/en/latest/matter/ep_contact_sensor.html
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    // 1. Initialize Matter
-    // Configure your Contact Sensor endpoint here...
-    
-    // 2. Read Switch
-    pinMode(SENSOR_PIN, INPUT_PULLUP);
-    bool isOpen = digitalRead(SENSOR_PIN);
-    
-    // 3. Update Matter Attribute
-    // Update the 'ContactStatus' attribute in the Matter cluster
-    
-    // 4. Configure Sleep
-    // Wake up when the pin level changes (e.g., HIGH to LOW)
-    #ifdef ESP32_S3_ENABLED 
-    esp_sleep_enable_ext0_wakeup((gpio_num_t)SENSOR_PIN, ESP_EXT1_WAKEUP_ANY_LOW);
-    #else
-    esp_deep_sleep_enable_gpio_wakeup(1 << SENSOR_PIN, ESP_GPIO_WAKEUP_GPIO_LOW);
-    #endif
-    
-    // 5. Sleep
-    Serial.println("Going to sleep...");
-    esp_deep_sleep_start();
+  // 1. Initialize Matter
+  // Configure your Contact Sensor endpoint here...
+
+  // 2. Read Switch
+  pinMode(buttonPin, INPUT_PULLUP);
+  bool isOpen = digitalRead(buttonPin);
+
+  // led
+  pinMode(ledPin, OUTPUT);
+
+  // CONFIG_ENABLE_CHIPOBLE is enabled when BLE is used to commission the Matter Network
+#if !CONFIG_ENABLE_CHIPOBLE  
+  // Manually connect to WiFi
+  WiFi.begin(ssid, password);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Wifi connected");
+#endif
+
+  // 3. Update Matter Attribute
+  // Update the 'ContactStatus' attribute in the Matter cluster
+  ContactSensor.begin();
+  digitalWrite(ledPin, LOW);  // LED OFF
+
+  // Matter beginning - Last step, after all EndPoints are initialized
+  Matter.begin();
+
+  // Check Matter Accessory Commissioning state, which may change during execution of loop()
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("");
+    Serial.println("Matter Node is not commissioned yet.");
+    Serial.println("Initiate the device discovery in your Matter environment.");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("QR code URL: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
+    // waits for Matter Contact Sensor Commissioning.
+    uint32_t timeCount = 0;
+    while (!Matter.isDeviceCommissioned()) {
+      delay(100);
+      if ((timeCount++ % 50) == 0) {  // 50*100ms = 5 sec
+        Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
+      }
+    }
+    Serial.println("Matter Node is commissioned and connected to the network. Ready for use.");
+  }
+
+// 4. Configure Sleep
+// Wake up when the pin level changes (e.g., HIGH to LOW)
+#ifdef ESP32_S3_ENABLED
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)(1 << SENSOR_PIN), ESP_EXT1_WAKEUP_ANY_LOW);
+#else
+  esp_deep_sleep_enable_gpio_wakeup(1 << SENSOR_PIN, ESP_GPIO_WAKEUP_GPIO_LOW);
+#endif
+
+  // 5. Sleep
+  Serial.println("Going to sleep...");
+  //esp_deep_sleep_start();
 }
 
 void loop() {
-    // This code will not be reached if you use deep sleep
+  // This code will not be reached if you use deep sleep
+  bool isOpen = digitalRead(buttonPin);
+  Serial.print("Testing the sketch state: ");
+  Serial.print(isOpen);
+  Serial.print(" / ");
+  Serial.println(ContactSensor.getContact());
+  sleep(1);
 }
